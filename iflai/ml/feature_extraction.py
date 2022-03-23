6b023a1d22ec9ab8795d9a78ca9f9458dd2ef2de
Ali <ali.boushehri@roche.com>
2022-03-23 13:15:17 +0100

    cleaned the feature_extraction file

--- iflai/ml/feature_extraction.py
+++ iflai/ml/feature_extraction.py
@@ -4,16 +4,36 @@ from joblib import Parallel, delayed
 from tqdm import tqdm
 from iflai.utils import list_of_dict_to_dict
 
+def get_image_mask(f):
+    """
+    f: full file path to the .h5 file
+    """
+    h5_file = h5py.File(f, "r")      
+    image = h5_file.get("image")[()]*1.
+    mask  = h5_file.get("mask")[()]
+    h5_file.close()
+    return image, mask
+
+
 class FeatureExtractor(object):
+    """
+    Feature extractor based on metadata dataframe.
+    it expects to get the metadata dataframe including file paths. 
+    """
     def __init__(self, feature_unions):
+        """
+        feature_unions is a series of sklearn transforms, using 
+        sklearn.pipeline.FeatureUnion
+        """
         self.feature_unions = feature_unions
 
     def extract_(self, f):
+        """
+        loads the images and masks based on the metadata["file"]
+        and pass the feature_unions on the data
+        """
         try:
-            h5_file = h5py.File(f, "r")      
-            image = h5_file.get("image")[()]*1.
-            mask  = h5_file.get("mask")[()]
-            h5_file.close()
+            image, mask = get_image_mask(f)
             features = self.feature_unions.transform([image,mask]).copy()
             features = list_of_dict_to_dict(features)
         except:
@@ -22,14 +42,10 @@ class FeatureExtractor(object):
         return features
 
     def extract_features(self, metadata, n_jobs = -1):
+        """
+        parallelization of feature extraction. It uses tqdm for progress
+        """
         file_list = metadata["file"].tolist()
         results = Parallel(n_jobs=n_jobs)(delayed(self.extract_)(f) \
             for f in tqdm(file_list, position=0, leave=True) )
         return results
-
-    def get_image_mask(self, metadata, i = 0):
-        h5_file = h5py.File(metadata.loc[i,"file"], "r")      
-        image = h5_file.get("image")[()]*1.
-        mask  = h5_file.get("mask")[()]
-        h5_file.close()
-        return image, mask
